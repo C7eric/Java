542799c2147ca54c4f44c11a2df15c0ff04af5c2
CSQ <eRicCSQ0611@outlook.com>
2022-04-17 22:22:55 +0800

    第一个java代码

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+//第一个Java程序
+//1.public class helloworld 表示hello是一个类,是一个public【共有】的类
+//2.public static void main(String[] args){} 表示一个主方法，即：程序的入口
+//3.System.out.println("Hello World!"); 表示输出“Hello World!" 到屏幕
+public class helloworld {
+
+    // 编写一个main方法
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(" World!");
+
+    }
+}

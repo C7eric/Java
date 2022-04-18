148c225d6a7a7b72e003947080956862249c0d66
CSQ <eRicCSQ0611@outlook.com>
2022-04-18 13:09:07 +0800

    while 语法 与 if 分支选择

---
+++ Loopy.java
@@ -0,0 +1,38 @@
+//循环示例
+public class Loopy {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println("Before the Loopy");
+        while (x < 4) {
+            System.out.println("In the Loopy");
+            System.out.println("Value of x is " + x);
+            x = x + 1;
+        }
+        System.out.println("This is after the Loop");
+    }
+}
+
+// 条件分支
+class IfTest {
+
+    public static void main(String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        }
+        System.out.println("This runs no matter what");
+    }
+}
+
+class IfTest2 {
+
+    public static void main(String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        } else {
+            System.out.println("x is NOT 3");
+        }
+        System.out.println("This runs no matter what");
+    }
+}

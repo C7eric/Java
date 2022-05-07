38a65797cb9d661fa619b34eaff782a256e8767d
CSQ <eRicCSQ0611@outlook.com>
2022-05-07 20:40:50 +0800

    方法练习2

---
+++ practice/MethodExercise02.java
@@ -0,0 +1,33 @@
+package Java.practice;
+
+public class MethodExercise02 {
+
+     public static void main(String[] args) {
+
+          Person p = new Person();
+          p.name = "CSQ";
+          p.age = 19;
+
+          Mytools tool = new Mytools();
+          Person p2 = tool.copyPerson(p);
+
+     }
+}
+
+class Person {
+          String name;
+          int age;
+}
+
+class Mytools {
+
+          public Person copyPerson(Person p){
+
+               Person p2 = new Person();
+               p2.name = p.name;
+               p2.age = p.age;
+               return p2;
+          }
+
+}
+

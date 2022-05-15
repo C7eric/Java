4c0cb6d9c6f27ed35c7154e63e372d577f121f8c
CSQ <eRicCSQ0611@outlook.com>
2022-05-15 14:46:53 +0800

    .

--- com/csqjava/poly_/Master.java
+++ com/csqjava/poly_/Master.java
@@ -16,12 +16,22 @@ public class Master {
         this.name = name;
     }
 
-    public void feed(Dog dog,Bone bone){
-        System.out.println("主人" + name + "给" + dog.getName() + "吃" + bone.getName());
+    /**
+     * 使用多态机制，可以统一的管理主人没事的问题
+     * animal 编译类型是Animal ,可以指向(接受) Animal 子类的对象
+     * food 编译类型的 Food ,可以指向(接受) Food 子类的对象
+     * @param animal
+     * @param food
+     */
+    public void feed(Animal animal,Food food){
+        System.out.println("主人" + name + "给" + animal.getName() + "吃" + food.getName());
     }
-
-    public void feed(Cat cat,Fish fish){
-        System.out.println("主人" + name + "给" + cat.getName() + "吃" + fish.getName());
-    }
+//    public void feed(Dog dog,Bone bone){
+//        System.out.println("主人" + name + "给" + dog.getName() + "吃" + bone.getName());
+//    }
+//
+//    public void feed(Cat cat,Fish fish){
+//        System.out.println("主人" + name + "给" + cat.getName() + "吃" + fish.getName());
+//    }
 
 }

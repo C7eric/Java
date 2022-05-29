2146e8471bb24e3e3924a8b2f728e7ee16094502
CSQ <eRicCSQ0611@outlook.com>
2022-05-29 21:47:03 +0800

    Changes

--- com/csqjava/notes_/A.java
+++
@@ -1,7 +0,0 @@
-package Java.com.csqjava.notes_;
-
-public class A {
-    public static void main(String[] args) {
-
-    }
-}
--- com/csqjava/notes_/TestDrive.java
+++
@@ -1,7 +0,0 @@
-package Java.com.csqjava.notes_;
-
-public class TestDrive {
-    public static void main(String[] args) {
-        System.out.println("csq");
-    }
-}
